50463b46c038f702d236c893a35161fae244133f
preethi-it19 <54267426+preethi-it19@users.noreply.github.com>
2019-10-10 12:10:53 +0530

    second commit

---
+++ ascending.c
@@ -0,0 +1,57 @@
+include <stdio.h>
+
+
+void main()
+
+{
+    
+        int a[5],i,j,temp;
+   
+        printf("enter 5 numbers");
+   
+        for(i=0;i<=5;i++)
+    
+           {
+        
+                scanf("%d",&a[i]);
+  
+            }
+    
+   
+      for (i = 0; i < 5; i++)
+	  
+       {
+       	
+           for (j = i + 1; j < 5; j++)
+	    
+                  {
+      	   
+                    if(a[i] > a[j])
+	       
+                     {
+            
+	       temp = a[i];
+             
+	       a[i] = a[j];
+	            
+                       a[j] = temp;
+        	
+                    }
+	
+    	
+                 }
+	 
+         }
+	
+         printf("assending order is\n");
+
+          for (i = 0; i < 5; i++)
+
+	{
+		
+                   printf("%d\t", a[i]);
+	
+                }
+
+}
+	
