156a993d208abf265d57a4a88fe535c047b59a92
preethi-it19 <54267426+preethi-it19@users.noreply.github.com>
2019-10-11 16:02:05 +0530

    commit 2

---
+++ string.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+ 
+void  main()
+
+{
+    
+   char s[10],i;
+
+    printf(" enter a string");
+
+    scanf("%s",s);
+
+    for(i=0;s[i]!='\0';i++)
+
+    {
+
+        printf("%c\n",s[i]);
+
+    }
+
+ 
+}
